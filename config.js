af7628e9854599ab71ae98230b1d1af1ed7aebd6
WhatsApp-tarboo <178101346+WhatsApp-tarboo@users.noreply.github.com>
2024-08-23 17:43:28 +0300

    Add files via upload

--- config.js
+++ config.js
@@ -1,66 +1,64 @@
-import {watchFile, unwatchFile} from 'fs';
 import chalk from 'chalk';
-import {fileURLToPath} from 'url';
-import fs from 'fs'; 
+import { fileURLToPath } from 'url';
+import fs from 'fs';
 import cheerio from 'cheerio';
 import fetch from 'node-fetch';
 import axios from 'axios';
 import moment from 'moment-timezone';
 
-global.botnumber = "+201151773932"
-global.confirmCode = ""
+moment.tz.setDefault("Africa/Cairo"); // تحديد التوقيت الزمني الافتراضي
+
+global.botnumber = "";
+global.confirmCode = "";
 
 global.owner = [
-  ['201151094460', '👑 Nano - Creador 👑', true],
-  ['201151773932'],
-  ['201559815566'],
-  ['201557561907'],
-  ['201227278285'],
-  ['201229419165'],
+  ['201225655220', ':┋𝐓𝐀𝐑𝐁𝐎𝐎 𝐎𝐖𝐍𝐄𝐑┋:', true],
+  ['201228616765', ':┋𝐓𝐀𝐑𝐁𝐎𝐎 𝐎𝐖𝐍𝐄𝐑²┋: ', true],
 ];
 
-global.suittag = ['201151773932'];
-global.prems = ['201151773932'];
+global.suittag = ['201225655220'];
+global.prems = ['201225655220'];
+global.packname = '• 𝙱𝙾𝚃 𝙴𝙻 𝚃𝙰𝚁𝙱𝙾𝙾 | 🐼❤️) ء';
+global.author = '‹ ᴛᴀʀʙᴏᴏ ʙᴏᴛ ،| 🌝♥️';
+global.vs = 'V2 • 1.0.5';
+global.wm = '• 🅃🄰🅁🄱🄾🄾 🄱🄾🅃 | ،💛؛❥!';
+global.titulowm = '•❬ 𝒕𝒂𝒓𝒃𝒐𝒐 𝒃𝒐𝒕 ❭•︎';
+global.titulowm2 = `• 𝙱𝙾𝚃 𝙴𝙻 𝚃𝙰𝚁𝙱𝙾𝙾 | 🐼❤️)`;
+global.igfg = '𝑇𝐴𝑅𝒃𝑂𝑂 𝒃𝑂𝑇';
+global.rwait = '⌛';
+global.done = '✅';
+global.wait = '⌛ *`الرجاء الانتظار قليلا...`*\n*✪✦✦⊹⊹⊹⊹⊹⊹⊹⊹✪*';
 
-global.packname = 'Sticker';
-global.author = 'The Nano - Bot';
-global.wm = 'The Nano - Bot';
-global.titulowm = 'The Nano - Bot';
-global.titulowm2 = `The Nano - Bot`
-global.igfg = 'The Nano - Bot';
-global.wait = '*[ ⏳ ] جار تلبيت طلبك...*';
-
-global.imagen1 = fs.readFileSync('./Menu.png');
-global.imagen2 = fs.readFileSync('./Menu.png');
-global.imagen3 = fs.readFileSync('./Menu.png');
+global.imagen1 = fs.readFileSync('./Menu2.jpg');
+global.imagen2 = fs.readFileSync('./src/nuevobot.jpg');
+global.imagen3 = fs.readFileSync('./src/Pre Bot Publi.png');
 global.imagen4 = fs.readFileSync('./Menu.png');
-global.imagen5 = fs.readFileSync('./Menu.png');
+global.imagen5 = fs.readFileSync('./src/+18.jpg');
 global.imagen6 = fs.readFileSync('./Menu3.png');
-global.imagen7 = fs.readFileSync('./Menu.png');
-global.imagen8 = fs.readFileSync('./Menu.png')
-global.imagen9 = fs.readFileSync('./Menu.png')
-global.imagen10 = fs.readFileSync('./Menu.png')
-
+global.imagen7 = fs.readFileSync('./src/menu_pt.png');
+global.imagen8 = fs.readFileSync('./src/menu_fr.png');
+global.imagen9 = fs.readFileSync('./src/menu_en.png');
+global.imagen10 = fs.readFileSync('./src/menu_ru.png');
 global.mods = [];
 
 //* *******Tiempo***************
-global.d = new Date(new Date + 3600000);
+global.d = new Date(new Date().getTime() + 3600000);
 global.locale = 'es';
-global.dia = d.toLocaleDateString(locale, {weekday: 'long'});
-global.fecha = d.toLocaleDateString('es', {day: 'numeric', month: 'numeric', year: 'numeric'});
-global.mes = d.toLocaleDateString('es', {month: 'long'});
-global.año = d.toLocaleDateString('es', {year: 'numeric'});
-global.tiempo = d.toLocaleString('en-US', {hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: true});
+global.dia = d.toLocaleDateString(locale, { weekday: 'long' });
+global.fecha = d.toLocaleDateString('es', { day: 'numeric', month: 'numeric', year: 'numeric' });
+global.mes = d.toLocaleDateString('es', { month: 'long' });
+global.año = d.toLocaleDateString('es', { year: 'numeric' });
+global.tiempo = d.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: true });
 //* ****************************
-global.wm2 = `${dia} ${fecha}\nThe Nano - Bot`;
-global.gt = 'The Nano - Bot';
-global.mysticbot = 'The Nano - Bot';
-global.md = 'https://github.com/BrunoSobrino/TheMystic-Bot-MD';
-global.mysticbot = 'https://github.com/BrunoSobrino/TheMystic-Bot-MD';
-global.waitt = '*[ ⏳ ] جاري تلبيت طلبك...*';
-global.waittt = '*[ ⏳ ] جاري تلبيت طلبك...*';
-global.waitttt = '*[ ⏳ ] جاري تلبيت طلبك...*';
-global.nomorown = '201151773932';
+global.wm2 = `${dia} ${fecha}\n𝑻𝑨𝑹𝑩𝑶𝑶 𝑩𝒐𝒕᪣︎`;
+global.gt = '𝑇𝐴𝑅𝒃𝑂𝑂 𝒃𝑂𝑇𐇵︎';
+global.mysticbot = '𝑇𝐴𝑅𝐵𝑂𝑂 𝐵𝑂𝑇᪣︎';
+global.md = '𝚃𝙰𝚁𝙱𝙾𝙾 𝙱𝙾𝚃💛📒 ؛';
+global.mysticbot = '𝚃𝙰𝚁𝙱𝙾𝙾 𝑩𝒐𝒕';
+global.waitt = '⌛ *`الرجاء الانتظار قليلا...`*\n*✪✦✦⊹⊹⊹⊹⊹⊹⊹⊹✪*';
+global.waittt = '⌛ *`الرجاء الانتظار قليلا...`*\n*✪✦✦⊹⊹⊹⊹⊹⊹⊹⊹✪*';
+global.waitttt = '⌛ *`الرجاء الانتظار قليلا...`*\n*✪✦✦⊹⊹⊹⊹⊹⊹⊹⊹✪*';
+global.nomorown = '20';
 global.pdoc = ['application/vnd.openxmlformats-officedocument.presentationml.presentation', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'application/vnd.openxmlformats-officedocument.wordprocessingml.document', 'application/vnd.ms-excel', 'application/msword', 'application/pdf', 'text/rtf'];
 global.cmenut = '❖––––––『';
 global.cmenub = '┊✦ ';
@@ -75,22 +73,23 @@ global.htki = '*⭑•̩̩͙⊱•••• ☪*';
 global.htka = '*☪ ••••̩̩͙⊰•⭑*';
 global.comienzo = '• • ◕◕════';
 global.fin = '════◕◕ • •';
-global.botdate = `*[ 📅 ] Fecha:*  ${moment.tz('America/Mexico_City').format('DD/MM/YY')}`;
-global.bottime = `*[ ⏳ ] Hora:* ${moment.tz('America/Mexico_City').format('HH:mm:ss')}`;
-global.fgif = {key: {participant: '0@s.whatsapp.net'}, message: {'videoMessage': {'title': wm, 'h': `Hmm`, 'seconds': '999999999', 'gifPlayback': 'true', 'caption': bottime, 'jpegThumbnail': fs.readFileSync('./Menu.png')}}};
+global.botdate = `*[ 📅 ] التاريخ:* ${moment.tz('Africa/Cairo').format('DD/MM/YY')}`;
+global.bottime = `*[ ⏳ ] الوقت:* ${moment.tz('Africa/Cairo').format('HH:mm:ss')}`;
+global.fgif = { key: { participant: '0@s.whatsapp.net' }, message: { 'videoMessage': { 'title': wm, 'h': `Hmm`, 'seconds': '999999999', 'gifPlayback': 'true', 'caption': bottime, 'jpegThumbnail': fs.readFileSync('./Menu.png') } } };
 global.multiplier = 99;
 global.flaaa = [
   'https://flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=water-logo&script=water-logo&fontsize=90&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextColor=%23000&shadowGlowColor=%23000&backgroundColor=%23000&text=',
   'https://flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=crafts-logo&fontsize=90&doScale=true&scaleWidth=800&scaleHeight=500&text=',
   'https://flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=amped-logo&doScale=true&scaleWidth=800&scaleHeight=500&text=',
   'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&text=',
-  'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&fillColor1Color=%23f2aa4c&fillColor2Color=%23f2aa4c&fillColor3Color=%23f2aa4c&fillColor4Color=%23f2aa4c&fillColor5Color=%23f2aa4c&fillColor6Color=%23f2aa4c&fillColor7Color=%23f2aa4c&fillColor8Color=%23f2aa4c&fillColor9Color=%23f2aa4c&fillColor10Color=%23f2aa4c&fillOutlineColor=%23f2aa4c&fillOutline2Color=%23f2aa4c&backgroundColor=%23101820&text=',
+  'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&fillColor1Color=%23f2aa4c&fillColor2Color=%23f2aa4c&fillColor3Color=%23f2aa4c&fillColor4Color=%23f2aa4c&fillColor5Color=%23f2aa4c&fillColor6Color=%23f2aa4c&fillColor7Color=%23f2aa4c&fillColor8Color=%23f2aa4c&fillColor9Color=%23f2aa4c&fillColor10Color=%23f2aa4c&fillOutlineColor=%23f2aa4c&fillOutline2Color=%23f2aa4c&backgroundColor=%23101820&text='
 ];
+
 //* ************************
 
 const file = fileURLToPath(import.meta.url);
-watchFile(file, () => {
-  unwatchFile(file);
+fs.watchFile(file, () => {
+  fs.unwatchFile(file);
   console.log(chalk.redBright('Update \'config.js\''));
   import(`${file}?update=${Date.now()}`);
 });
--- server.js
+++ server.js
@@ -1,49 +1,232 @@
-import express from 'express';
-import {createServer} from 'http';
-import path from 'path';
-import {Socket} from 'socket.io';
-import {toBuffer} from 'qrcode';
-import fetch from 'node-fetch';
+import pkg from '@whiskeysockets/baileys';
+const { generateWAMessageFromContent, proto, prepareWAMessageMedia } = pkg;
 
-function connect(conn, PORT) {
-  const app = global.app = express();
-  console.log(app);
-  const server = global.server = createServer(app);
-  let _qr = 'El código QR es invalido, posiblemente ya se escaneo el código QR.';
-
-  conn.ev.on('connection.update', function appQR({qr}) {
-    if (qr) _qr = qr;
-  });
-
-  app.use(async (req, res) => {
-    res.setHeader('content-type', 'image/png');
-    res.end(await toBuffer(_qr));
-  });
-
-  server.listen(PORT, () => {
-    console.log('[ ℹ️ ] La aplicación está escuchando el puerto', PORT, '(ignorar si ya escaneo el código QR)');
-    if (opts['keepalive']) keepAlive();
-  });
+function clockString(ms) {
+    let h = Math.floor(ms / 3600000);
+    let m = Math.floor(ms % 3600000 / 60000);
+    let s = Math.floor(ms % 60000 / 1000);
+    return [h, m, s].map(v => v.toString().padStart(2, '0')).join(':');
 }
 
-function pipeEmit(event, event2, prefix = '') {
-  const old = event.emit;
-  event.emit = function(event, ...args) {
-    old.emit(event, ...args);
-    event2.emit(prefix + event, ...args);
-  };
-  return {
-    unpipeEmit() {
-      event.emit = old;
-    }};
+const handler = async (m, { conn, usedPrefix, usedPrefix: _p, __dirname, text, isPrems }) => {
+    let d = new Date(new Date() + 3600000);
+    let locale = 'ar';
+    let week = d.toLocaleDateString(locale, { weekday: 'long' });
+    let date = d.toLocaleDateString(locale, { day: 'numeric', month: 'long', year: 'numeric' });
+    let _uptime = process.uptime() * 1000;
+    let uptime = clockString(_uptime);
+    let user = global.db.data.users[m.sender];
+    let name = conn.getName(m.sender);
+    let { money, joincount, diamond } = user;
+    let { exp, limit, level, role } = user;
+    let rtotalreg = Object.values(global.db.data.users).filter(user => user.registered === true).length;
+    let more = String.fromCharCode(8206);
+    let readMore = more.repeat(850);
+    let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender;
+    let taguser = '@' + m.sender.split("@s.whatsapp.net")[0];
+
+    await conn.sendMessage(m.chat, { react: { text: '📂', key: m.key } });
+
+    // الكود الخاص بالـ fakegif
+    let fakegif = {
+        key: { participant: `0@s.whatsapp.net`, ...(m.chat ? { remoteJid: m.chat } : {}) },
+        message: {
+            "videoMessage": {
+                "title": '🐱⸽⃕❬ 𝒃𝒐𝒕 𝒆𝒍 𝒕𝒂𝒓𝒃𝒐𝒐 ❭ - MD🍁⃨፝⃕✰',
+                "h": `Hmm`,
+                'seconds': '99999',
+                'gifPlayback': 'true',
+                'caption': '🐱⸽⃕❬ 𝒃𝒐𝒕 𝒆𝒍 𝒕𝒂𝒓𝒃𝒐𝒐 ❭ - MD🍁⃨፝⃕✰',
+                'jpegThumbnail': false
+            }
+        }
+    };
+
+    const images = [
+        'https://telegra.ph/file/bd87aef51ebbbba4901c8.jpg',
+        'https://telegra.ph/file/b9c7242b2ea534c9fea51.jpg',
+        'https://telegra.ph/file/0e611ef0f5898f84e06ff.jpg',
+        'https://telegra.ph/file/e40751a79e8f69137c772.jpg',
+        'https://telegra.ph/file/81ef617af171d1263bca4.jpg', 
+        'https://telegra.ph/file/9ece2dc7647c5bc552f7a.jpg', 
+        'https://telegra.ph/file/5a22e9d6a3db8a26c2a8d.jpg', 
+        'https://telegra.ph/file/5122cb52f3d3e6a15d27d.jpg', 
+        'https://telegra.ph/file/7d69133c3dae7d2cb988e.jpg', 
+        'https://telegra.ph/file/7af98c215f23a0c7bfc6a.jpg', 
+        'https://telegra.ph/file/e704ae1c0637553a0bff0.jpg', 
+        'https://telegra.ph/file/f4fe5a6340ca9f5890cb4.jpg'
+    ];
+
+    const randomImage = images[Math.floor(Math.random() * images.length)];
+
+    var messa = await prepareWAMessageMedia({ image: { url: randomImage } }, { upload: conn.waUploadToServer });
+
+    await conn.sendMessage(m.chat, { text: '*جاري تحضير قائمة الاوامر*' }, { quoted: fakegif });
+
+    await new Promise(resolve => setTimeout(resolve, 1000));
+
+    conn.relayMessage(m.chat, {
+        viewOnceMessage: {
+            message: {
+                interactiveMessage: {
+                    body: {
+                        text: `> *✧────[ 𝑾𝑬𝑳𝑪𝑶𝑴𝑬 ]────╮*
+> *┤ *مرحبا يا ${taguser}*
+> *┤ 🤴🏻 المطور: Mahmoud Mahmed*
+> *┤ #️⃣ الرقم: wa.me/201225655220*
+> *┤ ✅ الاصدار: 1.2.0*
+> *┤ 🎳 البادئة: •*
+> *┤ 🧜🏽‍♂️ المستخدمين: ${rtotalreg}*  
+> *┤────────────···*
+> *✧────[معـلـومـات الـمسـتـخـدم]────╮*
+> *┤ 🎩 *الاسـم: ${name}*
+> *┤ 🔃 المستوي: ${level}*
+> *┤ 🏆 *الـرتبة: ${role}*
+> *┤ 🎮 *الخبـرة: ${exp}* 
+> *┤ 💎 *الألـماس: ${diamond}* 
+> *┤ 🪙 *تربو كوينز: ${money}*
+> *┤ 🎟️ *الرموز: ${joincount}*
+> *┤ 🌟 *الـبـرﯾـمـيـوم: ${user.premiumTime > 0 ? 'مـمـيز✅' : (isPrems ? 'مـمـيز ✅' : 'عـادي ❌') || ''}* 
+> *┤────────────···* 
+> *✧────[ الـوقـت والـتـاريـخ ]────╮*
+> *┤ 📆 التاريخ: ${date}*
+> *┤ 📅 اليوم: ${week}*
+> *┤ 🚀 وقت النشاط: ${uptime}*
+> *┤────────────···*`
+                    },
+                    footer: {
+                        text: 'ᴹᴿ᭄𝒁𝒆𝒛𝒐ᴹᴿ᭄'
+                    },
+                    header: {
+                        title: '',
+                        hasMediaAttachment: true,
+                        imageMessage: messa.imageMessage,
+                    },
+                    nativeFlowMessage: {
+                        buttons: [
+{
+    name: 'single_select',
+    buttonParamsJson: JSON.stringify({
+        title: '『』CLICK《',
+        sections: [
+            {
+                title: '『』معلومات المطور《',
+                rows: [
+{
+    header: 'Developer Info',
+    title: '⌬ ❛╏المطور',
+    description: 'تعرف على المطور',
+    id: '.المطور'
+}
+                ]
+            },
+            {
+                title: '『』قوائم البرامج《',
+                rows: [
+{
+    header: 'Downloads',
+    title: '⌬ ❛╏التنزيلات',
+    description: 'جميع التحميلات هنا',
+    id: '.4',
+},
+{
+    header: 'Group Management',
+    title: '⌬ ❛╏قائمه الجروب',
+    description: 'إدارة الجروب',
+    id: '.5',
+},
+{
+    header: 'Games',
+    title: '⌬ ❛╏الالعاب',
+    description: 'ألعاب وترفيه',
+    id: '.6',
+}
+                ]
+            },
+            {
+                title: '『』الصور والادوات《',
+                rows: [
+{
+    header: 'Images',
+    title: '⌬ ❛╏الصور',
+    description: 'جميع الصور هنا',
+    id: '.2',
+},
+{
+    header: 'Tools',
+    title: '⌬ ❛╏الــادوات',
+    description: 'أدوات مفيدة',
+    id: '.7',
+}
+                ]
+            },
+            {
+                title: '『』أخرى《',
+                rows: [
+{
+    header: 'Guide',
+    title: '⌬ ❛╏شـرح الـالـقـاب',
+    description: 'شرح الألقاب',
+    id: '.3',
+},
+{
+    header: 'Terms',
+    title: '⌬ ❛╏شروط',
+    description: 'الشروط والأحكام',
+    id: '.20',
+},
+{
+    header: 'Support',
+    title: '⌬ ❛╏الدعم',
+    description: 'الدعم الفني والمساعدة',
+    id: '.الدعم',
+},
+{
+    header: 'All Commands',
+    title: '⌬ ❛╏قائمة الاوامر',
+    description: 'عرض جميع الأوامر المتاحة',
+    id: '.10',
+}
+                ]
+            }
+        ]
+    }),
+    messageParamsJson: 'TARBOO bot'
+},
+{
+    name: "quick_reply",
+    buttonParamsJson: "{\"display_text\":\"『』OWNER《\",\"id\":\".المطور\"}"
+},
+{
+    name: "quick_reply",
+    buttonParamsJson: '{"display_text":"⌈🚀╎الدردشه المجهوله╎🚀⌋","id":".chathom"}'
+},
+{
+    name: "cta_url",
+    buttonParamsJson: "{\"display_text\":\"『』GROUP《\",\"url\":\"https://chat.whatsapp.com/Gvj15Uocf6KDc2OUzgx06g\",\"merchant_url\":\"https://chat.whatsapp.com/Gvj15Uocf6KDc2OUzgx06g\"}"
+},
+{
+    name: "cta_url",
+    buttonParamsJson: "{\"display_text\":\"『』WEBSITE《\",\"url\":\"https://atom.bio/zyad_yasser\",\"merchant_url\":\"https://atom.bio/zyad_yasser\"}"
+},
+{
+    name: "cta_url",
+    buttonParamsJson: JSON.stringify({
+        display_text: "『』CHANNEL《",
+        url: "https://whatsapp.com/channel/0029Vaflefp4Y9ljqmqllP3a",
+        merchant_url: "https://whatsapp.com/channel/0029Vaflefp4Y9ljqmqllP3a"
+    })
+}
+                        ]
+                    }
+                }
+            }
+        }
+    }, {});
 }
 
-function keepAlive() {
-  const url = `https://${process.env.REPL_SLUG}.${process.env.REPL_OWNER}.repl.co`;
-  if (/(\/\/|\.)undefined\./.test(url)) return;
-  setInterval(() => {
-    fetch(url).catch(console.error);
-  }, 5 * 1000 * 60);
-}
+handler.help = ['info'];
+handler.tags = ['main'];
+handler.command = ['اوامر','الاوامر','menu','المهام'];
 
-export default connect;
+export default handler;
